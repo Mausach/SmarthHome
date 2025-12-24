033d1f624b321cad2ec8eb620eacaaab91ac5dbb
Mauro <maurogabriely@gmail.com>
2025-12-24 09:50:42 -0300

    pre final sin optimizar

--- src/pages/Home/Componentes/Packs.jsx
+++ src/pages/Home/Componentes/Packs.jsx
@@ -38,6 +38,8 @@ const Packs = () => {
                 "Cámara inteligente",
                 "Sensor de movimiento",
                 "Iluminación automática",
+                "Vision nocturna",
+                "Multi plataforma",
             ],
         },
         {
